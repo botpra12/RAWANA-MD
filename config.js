0581102a434be274d227e98f83605f79cdc26367
botpra12 <hackwge@gmail.com>
2025-06-22 20:57:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined? "JmdhBZTQ#C0IReJfWIFlo01DqAwmp8XZoG88LF2sjflMek-xvrL0":process.env.SESSION_ID,
-MONGODB: process.env.MONGODB || "mongodb://mongo:JIcieLhFtgGjVylHrNtCElhAYZUmKdaJ@hopper.proxy.rlwy.net:17262l",
+SESSION_ID: process.env.SESSION_ID === undefined? "8qt1TIQD#8SYujPsxvyQldgWXSg4-Fe5fHOMWUeyGSAqc4AJkoVo":process.env.SESSION_ID,
+MONGODB: process.env.MONGODB || "mongodb://mongo:RVhpFMrTIeIPxWtqRzDWUStJsMgUucxj@trolley.proxy.rlwy.net:28176",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
 repo: process.env.GITHUB_REPO || "RAWANA-MD",
